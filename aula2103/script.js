229d24ac237a125e18d3c8cc0dd1ad16e1053394
Vitor Rampazzi Franco <125324715+vitorrampazzi@users.noreply.github.com>
2025-03-21 21:04:50 -0300

    aula js

---
+++ aula2103/script.js
@@ -0,0 +1,77 @@
+// console.log('ME BLOQUEIA, DAS MENSAGENS E DA SUA VIDA')
+// console.info('onegai shimass')
+// console.warn('hello')
+// console.error('oi')
+
+// console.table([
+//     {id: 1, tarefa:'estudar js'},
+//     {id: 2, tarefa:'estuadr jssa'}
+// ])
+
+// console.group('grupo de logs')
+// console.log('1')
+// console.log('2')
+// console.groupEnd()
+
+// console.time('timer')
+// //processamento
+// console.timeEnd('timer')
+
+// //comentario de 1 linha 
+// var antigo = 'valor da variavel'
+
+// console.log(antigo)
+
+// let variavelMutavel = 'valor q pode ser alterado'
+
+// const variavelImutavel = 'valor q nao pode ser alterado'
+
+// variavelMutavel = 1
+
+// console.log(variavelImutavel)
+
+// variavelImutavel = 1 
+
+// console.log(variavelImutavel)
+
+// let texto = 'texto'
+// console.log(typeof texto)
+
+// let numero = 123
+// console.log(typeof numero)
+
+// let boolean = true
+// console.log(typeof boolean)
+
+// let semValor 
+// console.log(typeof semValor)
+
+// let nulo = null
+// console.log(typeof nulo)
+
+// let uniqueId = Symbol('id')
+// console.log(typeof uniqueId)
+
+// let bigNumero = 1233123321333312131232133n
+// console.log(typeof bigNumero)
+
+// let tarefa = {id:1, tarefa:'estudar JS'}
+// console.log(tarefa)
+// console.log(tarefa.id)
+// console.log(tarefa.tarefa)
+
+
+// let tasks = [
+//     {id:1, tarefa: 'estudar JS'},
+//     {id:2, tarefa: 'estudar JS'}
+// ]
+
+// console.log(tasks)
+// console.log(tasks[0])
+// console.log(tasks[0]['id'])
+
+// let hoje = new Date()
+// console.log(hoje)
+
+// let pattern = /^[a-z]+$/i;
+

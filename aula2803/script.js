439c0c4a219dbc2ba87e318b9d43b10fb965a2d8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-28 20:44:38 -0300

    aula 2803

---
+++ aula2803/script.js
@@ -0,0 +1,161 @@
+// let tasks = [
+//     { id: 1, descricao: "aula de JS" },
+//     { id: 2, descricao: "aula de HTML" },
+//     { id: 3, descricao: "aula de CSS" }
+// ]
+
+// console.log(tasks)
+// console.table(tasks)
+
+// console.log(tasks[0])
+// console.log(tasks[0]['descricao'])
+
+// console.log(tasks[1])
+// console.log(tasks[1]['descricao'])
+
+// let taskDescricao = ['aula', 'web', 'dev']
+// console.log(taskDescricao[0])
+
+// let task = {
+//     id: 1, descricao: "Aula de JS", data: new Date()
+// }
+
+// console.log(task)
+
+// let hoje = new Date()
+// let amanha = new Date("03-29-25")
+
+// console.log(amanha)
+// console.log(hoje)
+
+// let pattern = /^\+55\(\d{2}\)\d{4,5}\-\d{4}$/;
+// console.log(pattern.test("+55(11)92299-0899")); // true
+// console.log(pattern.test("+55(11)2299-0899"));  // true
+
+// let numero = 11;
+
+// let numeroEmTexto = String(numero)
+
+// console.log(numero, typeof numero)
+
+// console.log(numeroEmTexto, typeof numeroEmTexto)
+
+// let numeroEmTexto2 = '23'
+
+// console.log(numeroEmTexto2, typeof numeroEmTexto2)
+
+// let numeroEmTexto2Convertido = Number(numeroEmTexto2)
+
+// let numeroEmTexto2Convertido = parseInt(numeroEmTexto2)
+
+// console.log(numeroEmTexto2Convertido, typeof numeroEmTexto2Convertido)
+
+// let a = 3
+// let b = 10
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a ** b)
+
+// let contador = 0
+// let contador2 = 0
+
+// console.log(contador++)
+// console.log(++contador)
+
+// let x = 10
+
+// x += 5
+// console.log(x)
+// x -= 5
+// console.log(x)
+// x /= 5
+// console.log(x)
+// x *= 5
+// console.log(x)
+// x %= 5
+// console.log(x)
+// x **= 5
+// console.log(x)
+
+// console.log(10 == '10')
+// console.log(10 === '10')
+
+// console.log(5 != '5')
+// console.log(5 !== '5')
+
+// let idade = 18
+
+// let status = idade >= 18 ? "Maior de idade" :
+//     "menor de idade"
+
+// console.log(status)
+
+// let task = true
+
+// if (task) {
+//     console.log('a tarefa esta concluida')
+// }
+// else {
+//     console.log('tarefa nao concluida')
+// }
+
+// let tarefa = 1
+
+// if (tarefa = 1) {
+//     console.log('tarefa concluida')
+// }
+
+// else if (tarefa == 2) {
+//     console.log('tarefa nao esta concluida')
+// }
+
+// else (tarefa == 3){
+//     console.log('tarefa 3')
+// }
+
+// let diaSemana = 'segunda'
+
+// switch (diaSemana) {
+//     case 'segunda':
+//         console.log('hoje é segunda')
+//         break;
+
+//     case 'segunda':
+//         console.log('hoje é segunda')
+//         break;
+
+//     case 'segunda':
+//         console.log('hoje é segunda')
+//         break;
+
+//     default:
+//         console.log('nao é um dia da semana')
+//         break
+// }
+
+// for (let i = 0; i <= 20;) {
+//     console.log('contagem', i)
+
+//     i++
+// }
+
+
+// let contador = 1
+
+// while (contador <= 5) {
+//     console.log('oi')
+
+//     contador++
+// }
+
+
+// let numero = 5
+
+// do {
+//     console.log('o numero é', numero)
+//     numero++
+// } while (numero < 10)
